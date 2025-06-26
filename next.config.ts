d5acce01d70bc14726aedeccb30bed01fb79eaf8
니나노래 <77103814+ninanoray@users.noreply.github.com>
2025-06-26 11:20:10 +0900

    feat: next config 기본 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "localhost",
+      },
+    ],
+  },
+  //basePath: "/moidot",
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  async rewrites() {
+    return [
+      // {
+      //   source: "/api/:path*", // api 요청
+      //   destination: `${API_SERVER}/:path*`, // 프록시할 서버의 주소
+      // },
+    ];
+  },
 };
 
 export default nextConfig;
