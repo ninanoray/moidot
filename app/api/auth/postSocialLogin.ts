afbe152477805d6cfe019b8842a28ad8eb0904f5
니나노래 <77103814+ninanoray@users.noreply.github.com>
2025-07-23 16:06:29 +0900

    chore: signin callback 요청 정상화

--- app/api/auth/postSocialLogin.ts
+++ app/api/auth/postSocialLogin.ts
@@ -15,7 +15,7 @@ export async function postSocialLogin({
   email,
   provider,
 }: PostSocialLogin): Promise<LoginResponse> {
-  const url = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/auth/social-logi`;
+  const url = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/auth/social-login`;
   const data = {
     provider: provider,
     email: email,
