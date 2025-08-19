40f4eb82b4e5b0037b0f18ca4dcfab2f0b61faf6
니나노래 <77103814+ninanoray@users.noreply.github.com>
2025-08-19 13:56:48 +0900

    feat: 디스플레이 조정중

--- app/sidebar/sidebarWrapper.tsx
+++ app/sidebar/sidebarWrapper.tsx
@@ -28,7 +28,7 @@ export async function SidebarWrapper({
   return (
     <SidebarProvider defaultOpen={defaultOpen} className="mscreen">
       <HomeSidebar session={session} />
-      <SidebarInset className="mscreen">{children}</SidebarInset>
+      <SidebarInset className="min-h-max">{children}</SidebarInset>
     </SidebarProvider>
   );
 }
