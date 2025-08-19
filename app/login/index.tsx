d0a6903524de87525e387be695fec7fa0730068b
니나노래 <77103814+ninanoray@users.noreply.github.com>
2025-08-19 14:02:46 +0900

    feat: 디스플레이 조정 완료
    - bg-layout: "size-full min-h-screen p-0 px-2 sat flex flex-col"
    - SidebarInset className: "min-h-max"

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -14,7 +14,7 @@ const Login = () => {
   }
 
   return (
-    <div className="max-w-sm flex flex-col gap-6">
+    <div className="max-w-sm flex-center flex-col gap-6">
       <div className="flex flex-col items-center gap-2">
         <Image
           src="/images/moidot/moidot.png"
