2c7b12496500e96435a61c62b9170886883864c7
니나노래 <77103814+ninanoray@users.noreply.github.com>
2025-08-22 15:05:22 +0900

    feat: 기본색상 정의 추가

--- app/(home)/components/public/header/index.tsx
+++ app/(home)/components/public/header/index.tsx
@@ -1,5 +1,6 @@
 import { RippleButton } from "@/components/animate-ui/buttons/ripple";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 interface PublicHomeHeaderProps {
   scroll: number;
@@ -8,14 +9,17 @@ interface PublicHomeHeaderProps {
 const PublicHomeHeader = ({
   scroll: scrollPosition,
 }: PublicHomeHeaderProps) => {
+  const router = useRouter();
+
   return (
     <header
       className={cn(
-        "w-full px-3 sat sticky top-0 z-50 flex justify-between items-center trans-300",
+        "w-full px-3 sat sticky top-0 z-50 flex justify-between items-center trans-300 mobile",
         scrollPosition > 0 ? "bg-primary shadow-md" : "bg-transparent"
       )}
     >
       <h1
+        onClick={() => router.push("/")}
         className={cn(
           scrollPosition > 0
             ? "text-primary-foreground dark:text-card-foreground"
@@ -24,8 +28,10 @@ const PublicHomeHeader = ({
       >
         MOIDOT
       </h1>
-      <RippleButton variant="secondary">
-        <a href="/login">시작하기</a>
+      <RippleButton variant="secondary" className="p-0">
+        <a href="/login" className="size-full px-4 py-2">
+          시작하기
+        </a>
       </RippleButton>
     </header>
   );
--- app/(home)/components/public/main/index.tsx
+++ app/(home)/components/public/main/index.tsx
@@ -24,28 +24,28 @@ const PublicHomeMain = ({ scroll: scrollPosition }: PublicHomeMainProps) => {
             }),
           ]}
           className={cn(
-            "size-full bg-card rounded-lg shadow-lg [&_div]:size-full",
-            "[&_section]:size-full [&_section]:p-6 [&_section]:flex-center [&_section]:flex-col [&_section]:break-keep",
-            "[&_h1]:sm:text-4xl [&_h1]:text-3xl",
-            "[&_h2]:sm:text-3xl [&_h2]:text-2xl [&_h2]:text-secondary"
+            "size-full bg-card rounded-lg shadow-lg overflow-hidden mobile [&_div]:size-full",
+            "[&_section]:size-full [&_section]:p-6 [&_section]:flex-center [&_section]:flex-col [&_section]:break-keep [&_section]:rounded-lg [&_section]:trans-300",
+            "[&_h1]:sm:text-4xl [&_h1]:text-3xl [&_h1]:text-inherit",
+            "[&_h2]:sm:text-3xl [&_h2]:text-2xl [&_h2]:text-inherit"
           )}
           style={{ height: `calc(100% - ${scrollPosition}px)` }}
         >
           <CarouselContent className="max-w-[calc(100vw-16px)] m-0">
-            <CarouselItem className="w-auto p-0">
-              <section>
+            <CarouselItem className="p-0">
+              <section className="bg-tertiary text-tertiary-foreground">
                 <h2>모임</h2>
                 <h1>잇고 모여서, 계속 이어지는 모임</h1>
               </section>
             </CarouselItem>
-            <CarouselItem className="w-auto p-0">
-              <section>
+            <CarouselItem className="p-0">
+              <section className="bg-secondary text-secondary-foreground">
                 <h2>줄</h2>
                 <h1>순간과 순간을 잇는 스케-줄</h1>
               </section>
             </CarouselItem>
-            <CarouselItem className="w-auto p-0">
-              <section>
+            <CarouselItem className="p-0">
+              <section className="bg-primary text-primary-foreground">
                 <h2>닷</h2>
                 <h1>모든 순간의 시작</h1>
               </section>
@@ -56,16 +56,20 @@ const PublicHomeMain = ({ scroll: scrollPosition }: PublicHomeMainProps) => {
       <div className="w-full h-60 pb-2 shrink-0 flex gap-2.5 md:flex-row flex-col">
         <RippleButton
           size="lg"
-          className="flex-auto h-full text-3xl rounded-lg shadow-lg active:scale-98"
+          className="flex-auto h-full p-0 text-3xl rounded-lg shadow-lg active:scale-98"
         >
-          닷맵
+          <a href="/dotmap" className="size-full grid items-center">
+            닷맵
+          </a>
         </RippleButton>
         <RippleButton
           variant="secondary"
           size="lg"
-          className="flex-auto h-full text-3xl rounded-lg shadow-lg active:scale-98"
+          className="flex-auto h-full p-0 text-3xl rounded-lg shadow-lg active:scale-98"
         >
-          줄릿
+          <a href="#" className="size-full grid items-center">
+            줄릿
+          </a>
         </RippleButton>
       </div>
     </main>
