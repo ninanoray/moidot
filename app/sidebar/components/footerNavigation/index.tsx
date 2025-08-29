2437cb1301a9cd159e022d516c395ad2276e5a09
니나노래 <77103814+ninanoray@users.noreply.github.com>
2025-08-29 09:55:54 +0900

    feat: 메뉴바 조정중

--- app/sidebar/components/footerNavigation/index.tsx
+++ app/sidebar/components/footerNavigation/index.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Link from "next/link";
+
 const FooterNavigation = () => {
   return (
     <div className="md:sr-only relative bottom-0 shrink-0 w-full h-10 flex items-center justify-evenly bg-card">
-      <a href="/">홈</a>
+      <Link href="/">홈</Link>
       <a href="/dotmap">닷맵</a>
     </div>
   );
